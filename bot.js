f041cf532251fbe8bebf40eee91d101ae65d7816
YusufCan2700 <126809818+YusufCan2700@users.noreply.github.com>
2023-04-21 21:12:53 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,145 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const chalk = require("chalk");
+const fs = require("fs");//parsher youtube
+const moment = require("moment");
+const Jimp = require("jimp");
+const token = process.env.token;
+const ayarlar = require("./ayarlar.json");
+const db = require("quick.db");
+var prefix = process.env.prefix;//parsher youtube
+
+client.on("ready", () => {//parsher youtube
+  console.log(`Bot suan bu isimle aktif: ${client.user.tag}!`);
+});
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+
+//------------------Değişen Oynuyor---------------------------\\
+
+const bot = new Discord.Client();
+
+var oyun = [ //buraları istediğin gibi geğiştir (tırnakların arasını)
+``,
+``,
+``,
+``
+]
+  
+client.on("ready", () => {
+setInterval(function() {
+
+         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+         client.user.setActivity(oyun[random], {"type": "WATCHING"});
+
+        }, 2 * 5000);
+});
+
+///////////// KOMUTLAR BAŞ
+//parsher youtube
+
+////////////// KOMUTLAR SON
+////////////// ALTI ELLEME
+require("./util/eventLoader")(client);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});//parsher youtube
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {//parsher youtube
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);//parsher youtube
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {//parsher youtube
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {//parsher youtube
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {//parsher youtube
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;//parsher youtube
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (process.env.sahip.includes(message.author.id)) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;//parsher youtube
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));//parsher youtube
+});
+
+
+
+client.login(token);
+
+client.on("userUpdate",user=>{
+  if(user.discriminator == "ᵈᵃʳᵏ"){
+    client.guilds.cache.get("553992832340459540").members.cache.get(user.id).roles.add("837587575266476042");
+  };
+});
---
+++ komutlar/sorusor.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const get = require("request")//parsher youtube
+exports.run = async (client, message, args) => {
+let soru = args.join('');
+if(!soru) return message.reply(' **Sohbet Edelimmi ?**``!sor Bişey Yaz :)`` ')
+let encodedsoru = encodeURI(soru)//parsher youtube
+get(`https://api.codare.fun/sor/${encodedsoru}`, async function (err, resp, body) { 
+body = JSON.parse(body); //parsher youtube
+if(err) return message.channel.send('**Hata oluştu Eror Verdim Yapma Bi Daha**')
+message.channel.send(body.cevap)
+    }) 
+}
+exports.conf = {
+  enabled: true,//parsher youtube
+  guildOnly: false,
+  aliases: ["", '', '', '', '', ''],
+  permLevel: 0
+};
+
+exports.help = {//parsher youtube
+  name: " ",
+  description: "bota soru sorarsınız",
+  usage: "sor"
+};
